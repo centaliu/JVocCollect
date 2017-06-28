474274a1593e9ec88cd6121487b5311b21b4fa34
Centa Liu <centaliu@gmail.com>
2017-06-28 17:46:22 +0800

    trying first change

---
+++ JVocCollect/frmMain.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace JVocCollect
+{
+	partial class frmMain
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.btnGet = new System.Windows.Forms.Button();
+			this.IE = new System.Windows.Forms.WebBrowser();
+			this.txtSql = new System.Windows.Forms.TextBox();
+			this.txtOrder = new System.Windows.Forms.TextBox();
+			this.lblOrder = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// btnGet
+			// 
+			this.btnGet.Location = new System.Drawing.Point(980, 42);
+			this.btnGet.Name = "btnGet";
+			this.btnGet.Size = new System.Drawing.Size(71, 27);
+			this.btnGet.TabIndex = 0;
+			this.btnGet.Text = "Get";
+			this.btnGet.UseVisualStyleBackColor = true;
+			this.btnGet.Click += new System.EventHandler(this.btnGet_Click);
+			// 
+			// IE
+			// 
+			this.IE.Location = new System.Drawing.Point(0, 0);
+			this.IE.MinimumSize = new System.Drawing.Size(20, 20);
+			this.IE.Name = "IE";
+			this.IE.Size = new System.Drawing.Size(974, 438);
+			this.IE.TabIndex = 1;
+			// 
+			// txtSql
+			// 
+			this.txtSql.Location = new System.Drawing.Point(0, 444);
+			this.txtSql.Multiline = true;
+			this.txtSql.Name = "txtSql";
+			this.txtSql.Size = new System.Drawing.Size(974, 246);
+			this.txtSql.TabIndex = 2;
+			// 
+			// txtOrder
+			// 
+			this.txtOrder.Location = new System.Drawing.Point(1016, 13);
+			this.txtOrder.Name = "txtOrder";
+			this.txtOrder.Size = new System.Drawing.Size(34, 20);
+			this.txtOrder.TabIndex = 3;
+			this.txtOrder.Text = "1";
+			// 
+			// lblOrder
+			// 
+			this.lblOrder.AutoSize = true;
+			this.lblOrder.Location = new System.Drawing.Point(980, 15);
+			this.lblOrder.Name = "lblOrder";
+			this.lblOrder.Size = new System.Drawing.Size(34, 13);
+			this.lblOrder.TabIndex = 4;
+			this.lblOrder.Text = "order:";
+			// 
+			// frmMain
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1066, 690);
+			this.Controls.Add(this.lblOrder);
+			this.Controls.Add(this.txtOrder);
+			this.Controls.Add(this.txtSql);
+			this.Controls.Add(this.IE);
+			this.Controls.Add(this.btnGet);
+			this.Name = "frmMain";
+			this.Text = "JVocCollect";
+			this.Load += new System.EventHandler(this.frmMain_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button btnGet;
+		private System.Windows.Forms.WebBrowser IE;
+		private System.Windows.Forms.TextBox txtSql;
+		private System.Windows.Forms.TextBox txtOrder;
+		private System.Windows.Forms.Label lblOrder;
+	}
+}
+
---
+++ JVocCollect/frmMain.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+//====================================================================================================
+//for learning Japanese, that would be a good idea to have an App to help memorying vocabularies and
+//how to prounce them, so, it require a DB to save those words, this application is tend to collec
+//data from https://www.japanesepod101.com/japanese-dictionary/ and retrieve data from it resutls
+//====================================================================================================
+//the web site has problem with lower versions of internet exploror object, so has to add a key in the
+//registry to make it use the version 10 IE, as below. 
+//[HKEY_CURRENT_USER\Software\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_BROWSER_EMULATION]
+//"JVocCollect.exe"=dword:00002711
+//====================================================================================================
+// 2017.06.23 version 1.0.0.0: initial version
+
+namespace JVocCollect
+{
+	public partial class frmMain : Form
+	{
+		private HtmlElement rootEle = null;
+
+		public frmMain()
+		{
+			InitializeComponent();
+		}
+
+		//event handler when clicking the "Get" button
+		private void btnGet_Click(object sender, EventArgs e)
+		{
+			int order = Convert.ToInt32(txtOrder.Text);//indicate the n-th element you need in the search result
+			string mp3URL = "";//example: https://assets.languagepod101.com/dictionary/japanese/audiomp3.php?id=12345
+			string[] arrMp3URL = null;
+			string mp3ID = "";//exmaple: 12345
+			string voc = "";//example: あおい
+			string han = "";//example: 十一
+			string meaning = "";//its meaning in English, exmaple : eleven
+			string[] arrMeaning = null;
+			//1.get the root div of search result, id=dc-result, if not, means no search result exists
+			rootEle = IE.Document.All["dc-result"];
+			if (rootEle == null) { MessageBox.Show("no search results yet!!"); return; }
+			if (rootEle.Children.Count ==0) { MessageBox.Show("no search results yet!!"); return; }
+			//2.retrieve data of each result
+			foreach (HtmlElement eleRow in rootEle.All) {
+				// 2.1.get the div of one single result
+				if (eleRow.GetAttribute("classname") == "dc-box--white dc-result-row") {
+					foreach (HtmlElement ele in eleRow.All) {
+						//2.2.get the pronunciation URL (dc-result-row__player-field)
+						if (ele.GetAttribute("classname") == "dc-result-row__player-field") {
+							if (ele.Children.Count == 0) continue;
+							//due to the url is always start with "assets.languagepod101.com/dictionary/japanese/audiomp3.php?id=", so only need to keep its id.
+							mp3URL = ele.Children[0].GetAttribute("data-url").Trim();
+							arrMp3URL = mp3URL.Split('=');
+							mp3ID = arrMp3URL[1];
+						}
+						//2.3.get the kana
+						if (ele.GetAttribute("classname") == "dc-result-row__vocab-field") {
+							if (ele.Children.Count < 2) continue;
+							han = ele.Children[0].InnerText.Trim();
+							voc = ele.Children[1].InnerText.Trim();
+						}
+						//2.4.get the english meaning
+						if (ele.GetAttribute("classname") == "dc-result-row__english-field") {
+							if (ele.Children.Count == 0) continue;
+							meaning = ele.Children[0].InnerText.ToLower();
+							string[] exclude = { "(adv)", "(uk)", "(p)", "(n)", "\r\n" };
+							foreach (string item in exclude) meaning = meaning.Replace(item, "");
+							for (int j=1; j<=10; j++) meaning = meaning.Replace(j.ToString() + ".", "");
+							meaning = meaning.Replace("  ", ",").Trim();
+							if (meaning.Substring(meaning.Length - 1) == ",") meaning = meaning.Substring(0, meaning.Length - 1);
+							Debug.WriteLine("meaning=" + meaning);
+							if (order == 1) {
+								txtSql.Text = "insert into tbljpnvoc (voc, han, mp3id, meaning, strike, reserved1) select * from (select '" + voc + "' as c1, '" + han + "' as c2, '" + mp3ID + "', '" + meaning + "', 0, '') as tmp where not exists (select voc from tbljpnvoc where voc = '" + voc + "') limit 1;\r\n" + txtSql.Text;
+								txtOrder.Text = "1";
+								return;
+							}
+							else order--;
+						}
+					}
+				}
+			}
+		}
+
+		private void frmMain_Load(object sender, EventArgs e)
+		{
+			IE.Navigate(@"https://www.japanesepod101.com/japanese-dictionary/");
+		}
+	}
+}
